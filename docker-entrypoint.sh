7c7518cfecabff2cd3f6124ed6be72677567e2db
Hoang Nam (particle4dev) <particle4dev@gmail.com>
2020-07-29 12:05:56 +0700

    update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,18 +6,4 @@ then
   PORT=8888
 fi
 
-args="$@ -p $PORT"
-
-file=/data/db.json
-if [ -f $file ]; then
-    echo "Found db.json, trying to open"
-    args="$args db.json"
-fi
-
-file=/data/file.js
-if [ -f $file ]; then
-    echo "Found file.js seed file, trying to open"
-    args="$args file.js"
-fi
-
-json-server --host 0.0.0.0 $args
+json-server --host 0.0.0.0 -p $PORT $@
